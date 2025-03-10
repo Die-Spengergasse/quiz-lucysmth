972d1b7f959f46d0fd7e87bf31d76f3b9e229381
lucyj <smi220047@spengergasse.at>
2025-03-10 16:00:16 +0100

    Quiz angefangen, Klassen erstellt

---
+++ src/main/java/at/spengergasse/entities/Question.java
@@ -0,0 +1,43 @@
+package at.spengergasse.entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "q_questions")
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "q_id")
+    private int id;
+    @Column(name = "q_text")
+    private String text;
+
+    @Override
+    public String toString() {
+        return "Question " +
+                "[id=" + id + ", " +
+                "text=" + text +
+                "]";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<Answer> getAnswers() {
+
+    }
+}
